767f2b74a7442897bd2c92c03e209097fd967cd6
Marcelo F de Sousa <msousa@okn.com.br>
2020-04-19 16:28:14 -0300

    react build

---
+++ precache-manifest.cfc73f0128efe972d22f8abd9c367023.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "272b1de0204f7eb21780dbad49df8367",
+    "url": "/index.html"
+  },
+  {
+    "revision": "510b25b117444c0c6d13",
+    "url": "/static/css/main.5f6271b2.chunk.css"
+  },
+  {
+    "revision": "446f2effafaf32655d04",
+    "url": "/static/js/2.3b574d72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3b574d72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "510b25b117444c0c6d13",
+    "url": "/static/js/main.1730301d.chunk.js"
+  },
+  {
+    "revision": "60d4d3c6120e3d3d03da",
+    "url": "/static/js/runtime-main.719e621f.js"
+  },
+  {
+    "revision": "ff9673b4aba53e4cf5523a5855cc95f7",
+    "url": "/static/media/av.ff9673b4.jpg"
+  }
+]);
